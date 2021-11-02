2de9a1b6276e3948a7f6ab1bd76253cd4fb76e19
MihailChertin <Miha-chertin@yandex.ru>
2021-11-02 12:24:01 +0300

    Chenge Padding, Border, Margin.
    Padding has static methods for calcuate absolut indent.
    Margin has method to calculate for Border lines.

--- modules.py
+++ modules.py
@@ -1,14 +1,14 @@
 import pygame
-import pdb
 
 from itertools import cycle
 
 
 class Padding:
 	def __init__(self, size_space):
+		# pdb.set_trace()
 		if isinstance(size_space, int):
-			size_space = [size_space]*4
-		if isinstance(size_space, (list, tuple)):
+			self.spaces = [size_space]*4
+		elif isinstance(size_space, (list, tuple)):
 			if len(size_space) == 1:
 				self.spaces = size_space * 4
 			elif len(size_space) == 2:
@@ -17,27 +17,28 @@ class Padding:
 				self.spaces = size_space
 			else:
 				raise RuntimeError(f'Can\'t create \'{type(self).__name__}\'. Lengths \'{type(self).__name__}\' widths must be 1, 2, 4')
-		raise RuntimeError(f'Can\'t create \'{type(self).__name__}\'. {type(self).__name__}\'s widths must be list or tuple')
+		else:
+			raise RuntimeError(f'Can\'t create \'{type(self).__name__}\'. {type(self).__name__}\'s widths must be list or tuple')
 
 	def __getattr__(self, name):
 		if name == 'padding':
-			return self.size_space
+			return self.spaces
 
 	def __str__(self):
 		return f'top:{self.top}; rigth:{self.rigth}; bottom:{self.bottom}; left:{self.left};'
 
 	@property
 	def top(self):
-		return self.size_space[0]
+		return self.spaces[0]
 	@property
 	def rigth(self):
-		return self.size_space[1]
+		return self.spaces[1]
 	@property
 	def bottom(self):
-		return self.size_space[2]
+		return self.spaces[2]
 	@property
 	def left(self):
-		return self.size_space[3]
+		return self.spaces[3]
 
 	def horizontal_indent(self):
 		return self.rigth + self.left
@@ -45,8 +46,33 @@ class Padding:
 	def vertical_indent(self):
 		return self.top + self.bottom
 
+	@staticmethod
+	def absolut_vertical_indent(*spaces):
+		if len(spaces) > 3:
+			raise RuntimeError('To mani spaces for the object. Can be 3 scpaces or les.')
+		res = 0
+		for space in spaces:
+			if isinstance(space, Padding):
+				res += space.top + space.bottom
+			else:
+				raise RuntimeError(f'The {space} is not space. U can use Padding, Border, Margin')
+		return res
+
+	@staticmethod
+	def absolute_horizontal_indent(*spaces):
+		if len(space) > 3:
+			raise RuntimeError('To mani spaces for the object. Can be 3 scpaces or les.')
+		res = 0
+		for space in spaces:
+			if isinstance(space, Padding):
+				res += space.left + space.rigth
+			else:
+				raise RuntimeError(f'The {space} is not space. U can use Padding, Border, Margin')
+		return res
+
 class Border(Padding):
 	def __init__(self, border_widths, colors, parent):
+		# pdb.set_trace()
 		super().__init__(border_widths)
 		if parent is not None:
 			self._parent = parent
@@ -56,23 +82,23 @@ class Border(Padding):
 
 	def __getattr__(self, name):
 		if name == 'widths':
-			return self.size_space
+			return self.spaces
 
 	def __getitem__(self, key:int):
 		if isinstance(key, int) and key < 4:
 			if len(self.colors) == 3:
-				return self.size_space[key], self.colors
+				return self.spaces[key], self.colors
 			else:
-				return self.size_space[key], self.colors[key]
+				return self.spaces[key], self.colors[key]
 		else:
 			raise IndexError('Index must be int and les 4')
 
 	def __iter__(self):
 		if len(self.colors) == 3:
-			for line in self.size_space:
+			for line in self.spaces:
 				yield line, self.colors
 		else:
-			return zip(self.size_space, self.colors)
+			return zip(self.spaces, self.colors)
 
 	def __str__(self):
 		return super().__str__() + f' colors:{self.color};'
@@ -134,19 +160,13 @@ class Border(Padding):
 			margin = self.parent.margin
 		except AttributeError:
 			margin = Margin(0)
-		start_point = None
+		points_iter = margin.get_border_points()
+		start_point = next(points_iter)
 		counter = 0
-		for point in margin.get_border_points():
-			if start_point is None:
-				start_point = point
-				continue
-			try:
-				width, color = self[counter]
-			except IndexError:
-				break
-			pygame.draw.line(self.parent.background, start_point, point, color, width)
-			counter += 1
-			start_point = point
+		for line, color in self:
+			end_point = next(points_iter)
+			pygame.draw.line(self.parent.background, start_point, end_point, color, line)
+			start_point = end_point
 
 
 
@@ -156,7 +176,7 @@ class Margin(Padding):
 
 	def __getattr__(self, name):
 		if name == 'margin':
-			return self.size_space
+			return self.spaces
 
 	def get_border_points(self):
 		#self.left, self.top 		#3, 0
@@ -164,9 +184,17 @@ class Margin(Padding):
 		#self.rigth, self.bottom 	#1, 2
 		#self.left, self.bottom 	#3, 2
 		#self.left, self.top 		#3, 0
-		for y in cycle(self.size_space[::2]):
-			for x in cycle(self.size_space[-1::-2]):
-				yield x, y
+		switcher = True
+		y_iter = cycle(self.spaces[::2])
+		x_iter = cycle(self.spaces[-1::-2])
+		x = next(x_iter)
+		while True:
+			if switcher:
+				y = next(y_iter)
+			else:
+				x = next(x_iter)
+			yield (x, y)
+			switcher = not switcher
 
 class FontProperty:
 	def __init__(self, font_name=None, font_size=None, font_color=None):
@@ -227,7 +255,31 @@ class SizeRange:
 		return self.range_height[1]
 
 if __name__ == '__main__':
-	pygame.font.init()
-	font = FontParoperty(None, 35, (0, 0, 4))
-	font.create_font()
-	print(font.size)
+	import sys
+	margin = Margin([0, 1, 2, 3])
+	padding = Padding([1, 2])
+	marg = Margin(1)
+	# m = True
+	# print(margin.margin[-1::-2])
+	# print(margin.margin[::2])
+	# y_iter = cycle(margin.margin[::2])
+	# x_iter = cycle(margin.margin[-1::-2])
+	# x = next(x_iter)
+	# i = 0
+	# while i != 5:
+	# 	if m:
+	# 		y = next(y_iter)
+	# 	else:
+	# 		x = next(x_iter)
+	# 	print(x, y)
+	# 	m = not m
+	# 	i += 1
+	print(margin)
+	print(padding)
+	print(marg)
+	counter = 0
+	for x, y in margin.get_border_points():
+		print(x, y)
+		counter += 1
+		if counter == 5:
+			break
