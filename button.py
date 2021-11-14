46fe46cdc27a0a6c2f36d6be2c8abc585494027e
MihailChert <miha-chertin@yandex.ru>
2021-11-14 19:08:55 +0300

    rebase on linux

--- button.py
+++ button.py
@@ -1,62 +1,168 @@
+"""Summary
+"""
+from typing import Union, Optional, Any, Tuple, List, Iterable, Callable
 import pygame
-
 from lable import Lable
-from modules import Padding, Border, Margin
+from modules import Padding, Margin, SizeRange
+
+# pylint: disable=E1101
+
 
 class Button(Lable):
-	ID = 0
-	BUTTONEVENT = pygame.event.custom_type()
 
-	def __init__(self, parent, pos, font, text, background, text_align,
-				transparency=False, rect_size=None, size_range=None,
-				padding=Padding(10), target=None):
-		super().__init__(parent, pos, font, text, background,
-						text_align, transparency, rect_size,
-						size_range, padding)
-		Lable.ID -= 1
-		self.id = Button.ID
-		self.name = 'Button' + str(self.id)
-		Button.ID += 1
-		self.event = pygame.event.Event(Button.BUTTONEVENT)
-		self.event.button = self
-		self.event.button_id = self.id
-		self.event.button_name = self.name
-		self.event.target = target
-		self._pressed = False
+    """Summary
 
-	@property
-	def ispressed(self):
-		return self._pressed
+    Attributes
+    ----------
+    ID : int
+        Description
+    BUTTONEVENT : TYPE
+        Description
+    id : TYPE
+        Description
+    name : TYPE
+        Description
+    event : TYPE
+        Description
+    """
 
+    ID = 0
+    BUTTONEVENT = pygame.event.custom_type()
 
-	def collide(self, mouse_event, chenge_curr=False):
-		if self.rect.collidepoint(mouse_event.pos):
-			pygame.mouse.set_system_cursor(pygame.SYSTEM_CURSOR_HAND)
-			if mouse_event.type == pygame.MOUSEBUTTONDOWN or mouse_event.type == pygame.MOUSEBUTTONUP:
-				if self._pressed ^ mouse_event.type == pygame.MOUSEBUTTONDOWN:
-					pygame.event.post(self.event)
-				self._pressed = mouse_event.type == pygame.MOUSEBUTTONDOWN
-			return True
-		else:
-			if not chenge_curr:
-				pygame.mouse.set_system_cursor(pygame.SYSTEM_CURSOR_ARROW)
-			self._pressed = False
-			return chenge_curr
+    def __init__(
+        self,
+        parent,
+        pos: Iterable,
+        font: Optional[Tuple[str, int, pygame.Color]],
+        text: str,
+        background: Any,
+        text_align: str,
+        transparency: bool = False,
+        rect_size: Union[pygame.Rect, List[int], Tuple[int, int, int, int]] = None,
+        size_range: SizeRange = None,
+        padding: Padding = Padding(10),
+        borders: Union[int, List[int], Tuple[int, ...]] = 2,
+        border_color: Union[pygame.Color, Tuple[int, int, int]] = (255, 255, 255),
+        margin: Margin = Margin(0),
+        target: Optional[Callable] = None,
+    ):
+        """Summary
 
-	def draw(self):
-		if not self._pressed:
-			self.draw_unpressed()
-		else:
-			self.draw_pressed()
+        Parameters
+        ----------
+        parent : pygame.sprite.Sprite
+            Description
+        pos : Tuple[int, int]
+            Description
+        font : Union[FontProperty, dict, list, None]
+            Description
+        text : str
+            Description
+        background : Tuple[int, int, int]
+            Description
+        text_align : str
+            Description
+        trancparency : bool, optional
+            Description
+        rect_size : Union[list, tuple], optional
+            Description
+        size_range : SizeRange, optional
+            Description
+        padding : Padding, optional
+            Description
+        border : int, optional
+            Description
+        border_colors : Union[List[int], Tuple[int, int, int]], optional
+            Description
+        margin : Margin, optional
+            Description
+        """
+        super().__init__(
+            parent,
+            pos,
+            font,
+            text,
+            background,
+            text_align,
+            transparency,
+            rect_size,
+            size_range,
+            padding,
+            borders,
+            border_color,
+            margin,
+        )
+        Lable.ID -= 1
+        self.id = Button.ID
+        self.name = 'Button' + str(self.id)
+        Button.ID += 1
+        self.event = pygame.event.Event(Button.BUTTONEVENT)
+        self.event.button = self
+        self.event.button_id = self.id
+        self.event.button_name = self.name
+        self.event.target = target
+        self._pressed = False
 
-	def draw_unpressed(self):
-		self.surface.set_alpha(255)
-		self.parent.blit(self.surface, self.rect)
-		self.surface.fill(self.surface_color)
-		self.draw_text()
+    @property
+    def ispressed(self) -> bool:
+        """Summary
 
-	def draw_pressed(self):
-		self.surface.set_alpha(100)
-		self.parent.blit(self.surface, self.rect)
-		self.surface.fill(self.surface_color)
-		self.draw_text()
+        Returns
+        -------
+        bool
+            Description
+        """
+        return self._pressed
+
+    def collide(
+        self, mouse_event: pygame.event.Event, chenge_curr: bool = False
+    ) -> bool:
+        """Summary
+
+        Parameters
+        ----------
+        mouse_event : pygame.event.Event
+            Description
+        chenge_curr : bool, optional
+            Description
+
+        Returns
+        -------
+        bool
+            Description
+        """
+        if self.client_rect.collidepoint(mouse_event.pos):
+            pygame.mouse.set_system_cursor(pygame.SYSTEM_CURSOR_HAND)
+            if (
+                mouse_event.type == pygame.MOUSEBUTTONDOWN
+                or mouse_event.type == pygame.MOUSEBUTTONUP
+            ):
+                if self._pressed ^ (mouse_event.type == pygame.MOUSEBUTTONDOWN):
+                    pygame.event.post(self.event)
+                self._pressed = mouse_event.type == pygame.MOUSEBUTTONDOWN
+            return True
+        if not chenge_curr:
+            pygame.mouse.set_system_cursor(pygame.SYSTEM_CURSOR_ARROW)
+        self._pressed = False
+        return chenge_curr
+
+    def draw(self) -> None:
+        """Summary"""
+        if not self._pressed:
+            self.draw_unpressed()
+        else:
+            self.draw_pressed()
+
+    def draw_unpressed(self) -> None:
+        """Summary"""
+        self.surface.set_alpha(255)
+        self.parent.blit(self.surface, self.client_rect)
+        self.surface.fill(self.surface_color)
+        self.draw_text()
+
+    def draw_pressed(self) -> None:
+        """Summary"""
+        self.surface.set_alpha(100)
+        self.parent.blit(self.surface, self.client_rect)
+        self.surface.fill(self.surface_color)
+        self.draw_text()
--- dropdownlist.py
+++ dropdownlist.py
@@ -2,5 +2,15 @@ import pygame
 
 from button import Button
 
+
 class DropDownList:
-	pass
+    COUNTER = 0
+
+    def __init__(self, parent, pos, size_range):
+        self._id = DropDownList.COUNNTER
+        DropDownList.COUNTER += 1
+
+
+class DropDownListNode(Button):
+    def draw_text(self):
+        pass
