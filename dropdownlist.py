36c49fdb35775a1c2f5025b939b3ac9ea376a039
MihailChert <miha-chertin@yandex.ru>
2021-11-28 20:18:37 +0300

    Some update dropdown list

--- dropdownlist.py
+++ dropdownlist.py
@@ -4,43 +4,62 @@ from modules import FontProperty, Padding
 
 
 class DropDownList(pygame.sprite.Sprite):
-	COUNTER = 0
-	EVENT_ID = pygame.event.custom_type()
-	default_font_color = (0, 0, 0)
+    COUNTER = 0
+    EVENT_ID = pygame.event.custom_type()
+    default_font_color = (0, 0, 0)
 
-	def __init__(self, parent, font, pos, beckground_color=(255, 255, 255),
-				item_padding=Padding(10)):
-		pygame.sprite.Sprite.__init__(self)
-		self._id = DropDownList.COUNNTER
-		DropDownList.COUNTER += 1
-		self.set_font(font)
-		self.name = 'DropDownList' + str(self._id)
-		self.item_size = item_size
-		self.items = []
-		self._event = DropDownList._create_event()
-		self.rect = pygame.Rect(pos, (0, 0))
-		self.surface = pygame.Surface()
-		self.surface_color = beckground_color
+    def __init__(
+        self,
+        parent,
+        font,
+        pos,
+        beckground_color=(255, 255, 255),
+        item_padding=Padding(10),
+        max_width=0,
+    ):
+        pygame.sprite.Sprite.__init__(self)
+        self.visible = True
+        self._id = DropDownList.COUNNTER
+        DropDownList.COUNTER += 1
+        self.set_font(font)
+        self.name = "DropDownList" + str(self._id)
+        self.item_size = item_size
+        self.items = []
+        self.selected_item = None
+        self._event = DropDownList._create_event()
+        self.rect = pygame.Rect(pos, (0, 0))
+        self.max_width = max_width
+        self.surface = pygame.Surface()
+        self.surface_color = beckground_color
 
-	def set_font(self, font):
-		if isinstance(font, FontProperty):
-			self.font = font
-		elif isinstance(font, dict):
-			self.font = FontProperty(**font)
-		elif isinstance(font, (tuple, list)):
-			self.font = FontProperty(*font)
-		else:
-			self.font = FontProperty(None, 16, DropDownList.default_font_color)
-		self.font.create_font()
+    def set_font(self, font):
+        if isinstance(font, FontProperty):
+            self.font = font
+        elif isinstance(font, dict):
+            self.font = FontProperty(**font)
+        elif isinstance(font, (tuple, list)):
+            self.font = FontProperty(*font)
+        else:
+            self.font = FontProperty(None, 16, DropDownList.default_font_color)
+        self.font.create_font()
 
-	@statikmethod
-	def _create_event():
+    def _create_event(self):
+        if self._event is not None:
+            return self._event
+        event = pygame.event.Event(DropDownList.EVENT_ID)
+        event.list_name = self.name
+        event.list_id = self._id
+        event.item_id = None
+        return event
 
-	def add_item(self, item_name, item_size=(0, 0)):
-		self.items.append(item_name)
-		self.rect.h += self.item_size.h
-		self.rect.w = max(self.item_size)
-		self.draw()
+    def add_item(self, item_name, item_width=0):
+        self.items.append(item_name)
+        self.rect.h += self.font.get_height() + self.item_padding.vertical_indent()
+        self.rect.w = max(item_width, self.font.size(item_name)[0], self.rect.width)
+        if self.max_width > 0 and self.rect.width > self.max_width:
+            self.rect.width = self.max_width
+        self.draw()
 
-	def draw(self):
-
+    def draw(self):
+        if self.visible:
+            pass
