f372780db4fe2ff6a8cb78ca60e73e7b69cb97dd
MihailChert <Miha-chertin@yandex.ru>
2023-07-03 22:49:10 +0400

    feat: Text Drawing
    add controller and factory for text
    text controller dont have event_id, it use event_id from drawing controller

--- app/Drawing/TextDrawing/textcontroller.py
+++ app/Drawing/TextDrawing/textcontroller.py
@@ -7,7 +7,7 @@ class TextController(AbstractController):
     def __init__(self, factory):
         super().__init__(factory)
         self.__drawing_controller = factory.get_drawing_factory().get_controller()
-        self._event = self.__drawing_controller.get_event_id()
+        self._event_id = self.__drawing_controller.get_event_id()
 
     def add_listener_to(self, listened_method, handler):
         if listened_method == 'empty_method':
@@ -17,3 +17,10 @@ class TextController(AbstractController):
 
     def destroy(self, event):
         pygame.font.quit()
+
+    def create_event(self, method, event_attrs):
+        method = 'text_' + method
+        self.__drawing_controller.create_event(method, event_attrs)
+
+    def find_object(self, needle_object):
+        pass
--- app/Drawing/TextDrawing/textfactory.py
+++ app/Drawing/TextDrawing/textfactory.py
@@ -5,24 +5,31 @@ from .textcontroller import TextController
 
 class TextFactory(AbstractFactory):
 
-    def __init__(self, name, main_factory, config):
-        super().__init__(name, main_factory)
-        self._controller = None
+	def __init__(self, name, main_factory, config):
+		super().__init__(name, main_factory)
+		self._controller = None
 
-    @classmethod
-    def init(cls, name, main_factory, config):
-        pygame.font.init()
-        text = cls(name, main_factory, config)
-        text.logger.info('init text factory')
+	@classmethod
+	def init(cls, name, main_factory, config):
+		pygame.font.init()
+		text = cls(name, main_factory, config)
+		text.logger.info('init text factory')
 
-    @staticmethod
-    def get_default_config():
-        return {}
+	@staticmethod
+	def get_default_config():
+		return {}
 
-    def get_controller(self):
-        if self._controller is None:
-            self._controller = TextController(self)
-        return self._controller
+	def get_controller(self):
+		if self._controller is None:
+			self._controller = TextController(self)
+		return self._controller
 
-    def get_drawing_factory(self):
-        return self._main_factory.factories['drawing']
+	def get_drawing_factory(self):
+		return self._main_factory.factories['drawing']
+
+	@staticmethod
+	def get_font(font, font_size):
+		return pygame.font.Font(font, font_size)
+
+	def get_font_loader(self, source):
+		pass
